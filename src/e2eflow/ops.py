655f1b12ffed8bbdb9fd92a37daf4d16889ab31d
Claus <clauslang@yahoo.de>
2018-04-23 15:50:23 +0200

    removed google cuda macro oprion in nvcc command

--- src/e2eflow/ops.py
+++ src/e2eflow/ops.py
@@ -39,7 +39,8 @@ def compile(op=None):
         else:
             cuda_lib64_path_arg = ""
 
-        nvcc_cmd = "nvcc -std=c++11 -c -o {} {} {} -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -I /usr/local --expt-relaxed-constexpr"
+        # nvcc_cmd = "nvcc -std=c++11 -c -o {} {} {} -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -I /usr/local --expt-relaxed-constexpr"
+        nvcc_cmd = "nvcc -std=c++11 -c -o {} {} {} -x cu -Xcompiler -fPIC -I /usr/local --expt-relaxed-constexpr"
         nvcc_cmd = nvcc_cmd.format(" ".join([fn_cu_o, fn_cu_cc]),
                                    tf_inc, tf_lib)
         print()
