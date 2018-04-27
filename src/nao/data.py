911d514266a0f2c7957667ba93bc62fdc00f0521
Claus <clauslang@yahoo.de>
2018-04-27 13:02:09 +0200

    added nao data scripts

---
+++ src/nao/data.py
@@ -0,0 +1,19 @@
+import os
+
+from ..core.data import Data
+from ..util import tryremove
+
+
+class NaoData(Data):
+
+    def __init__(self, data_dir, stat_log_dir=None,
+                 development=True, fast_dir=None):
+        super().__init__(data_dir, stat_log_dir,
+                         development=development,
+                         fast_dir=fast_dir)
+
+    def _fetch_if_missing(self):
+        print('ERROR: Could not find nao data')
+
+    def get_raw_dirs(self):
+        return ['nao_raw/blue200']
