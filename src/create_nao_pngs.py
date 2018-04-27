f43d6c9ffa39ef2c60d4284b9d28758e94785efb
Claus <clauslang@yahoo.de>
2018-04-27 11:36:47 +0200

    finished png extraction code

--- src/create_nao_pngs.py
+++ src/create_nao_pngs.py
@@ -1,13 +1,16 @@
-import os
 import gzip
 import pickle
+from PIL import Image
 
 
 def main(pkl_file_path, destination_dir):
     with gzip.open(pkl_file_path) as src_file:
         memories = pickle.load(src_file, encoding="bytes")
-        i = 9
+        for i, memory in enumerate(memories):
+            name = destination_dir + "nao_img_" + str(i).zfill(6) + ".png"
+            image = memory[b'image'][:, :, ::-1]    # convert from BGR to RGB
+            Image.fromarray(image).save(name)
 
 
 if __name__ == "__main__":
-    main(pkl_file_path="../blue200_original.pkl", destination_dir="data/nao_raw/blue200")
+    main(pkl_file_path="../blue200_original.pkl", destination_dir="../data/nao_raw/blue200/")
