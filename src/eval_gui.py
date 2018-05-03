1f3ff542e0c3a4661c7b34ced7ce331b23e4b3b6
Claus <clauslang@yahoo.de>
2018-05-03 17:50:44 +0200

    debug

--- src/eval_gui.py
+++ src/eval_gui.py
@@ -306,7 +306,7 @@ def main(argv=None):
         data = NaoData(dirs['data'])
         data_input = NaoInput(data=data, batch_size=1, normalize=False,
                               # dims=(192, 256))
-                              dims=(256, 320))
+                              dims=(240, 320))
     elif FLAGS.dataset == 'chairs':
         data = ChairsData(dirs['data'], development=True)
         data_input = ChairsInput(data, batch_size=1, normalize=False,
