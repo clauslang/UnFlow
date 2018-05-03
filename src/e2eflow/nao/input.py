5c7af952e6d7cdd861b6a64367f09f2983c0fdfe
Claus <clauslang@yahoo.de>
2018-05-03 18:53:23 +0200

    debug

--- src/e2eflow/nao/input.py
+++ src/e2eflow/nao/input.py
@@ -6,6 +6,4 @@ import tensorflow as tf
 class NaoInput(Input):
 
     def input_test(self, hold_out_inv=None):
-        ret = self._input_test('nao_raw/blue200', hold_out_inv)
-        print('finished _input_test')
-        return ret
+        return self._input_test('nao_raw/blue200', hold_out_inv)
