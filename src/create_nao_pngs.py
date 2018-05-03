1739b760bf5fdbede4ad0d2f3dfaf2c8b070d79f
Claus <clauslang@yahoo.de>
2018-05-03 12:11:34 +0200

    printing progress status

--- src/create_nao_pngs.py
+++ src/create_nao_pngs.py
@@ -5,9 +5,14 @@ from PIL import Image
 
 def main(pkl_file_path, destination_dir):
     # todo: print status
+    print('opening file...')
     with gzip.open(pkl_file_path) as src_file:
+        print('loading images...')
         memories = pickle.load(src_file, encoding="bytes")
+        print('extracting {} images..'.format(len(memories)))
         for i, memory in enumerate(memories):
+            if i % 100 == 0:
+                print('extracted {} images'.format(i))
             name = destination_dir + "nao_img_" + str(i).zfill(6) + ".png"
             image = memory[b'image'][:, :, ::-1]    # convert from BGR to RGB
             Image.fromarray(image).save(name)
