15f21c63b77a5a83e62dc0693a5cbdf845b7c4c9
Claus <clauslang@yahoo.de>
2018-05-03 17:14:02 +0200

    debug

--- src/e2eflow/nao/input.py
+++ src/e2eflow/nao/input.py
@@ -5,11 +5,6 @@ import tensorflow as tf
 
 class NaoInput(Input):
 
-    def input_test(self):
-        input_shape, im1, im2 = self._input_test('nao_raw/blue200')
-        return tf.train.batch(
-            [im1, im2, input_shape],
-            batch_size=self.batch_size,
-            num_threads=self.num_threads,
-            allow_smaller_final_batch=True)
+    def input_test(self, hold_out_inv=None):
+        return self._input_test('data/nao_raw/blue200', hold_out_inv)
 
