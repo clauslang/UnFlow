33d02e1dca43ccd73331aba86cfa89dfa772c83a
Claus <clauslang@yahoo.de>
2018-05-04 18:05:16 +0200

    duplicating eval data fn

--- src/e2eflow/nao/input.py
+++ src/e2eflow/nao/input.py
@@ -5,3 +5,6 @@ class NaoInput(Input):
 
     def input_test(self, hold_out_inv=None):
         return self._input_test('nao_raw/grey400', hold_out_inv)
+
+    def input_train_2012(self, hold_out_inv=None):
+        return self._input_test('nao_raw/grey400', hold_out_inv)
