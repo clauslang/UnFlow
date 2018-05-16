9ef140d9284ad6dc50336c944601ac9fc41ede01
Claus <clauslang@yahoo.de>
2018-05-16 18:04:17 +0200

    debug

--- src/e2eflow/core/flow_util.py
+++ src/e2eflow/core/flow_util.py
@@ -31,7 +31,7 @@ def flow_to_color(flow, mask=None, max_flow=None):
     flow_u, flow_v = tf.unstack(flow, axis=3)
 
     print()
-    print(tf.reduce_min(flow).eval(tf.Session()))
+    print(tf.reduce_min(flow).eval(sess=tf.Session()))
     print()
 
     if max_flow is not None:
