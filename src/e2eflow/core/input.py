87fa1eb62b5decfdd260651804b48f33532752f5
Claus <clauslang@yahoo.de>
2018-05-16 14:33:49 +0200

    debug

--- src/e2eflow/core/input.py
+++ src/e2eflow/core/input.py
@@ -101,8 +101,8 @@ class Input():
             filenames_1 = list(filenames_1)
             filenames_2 = list(filenames_2)
 
-        print(filenames_1)
-        print(filenames_2)
+        print(len(filenames_1))
+        print(len(filenames_2))
 
         input_1 = read_png_image(filenames_1, 1)
         input_2 = read_png_image(filenames_2, 1)
