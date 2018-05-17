e94b451be1d28771b229da6094313d8aefe1bf49
Claus <clauslang@yahoo.de>
2018-05-17 17:25:51 +0200

    method for pickling flow images

--- src/nao_img_util.py
+++ src/nao_img_util.py
@@ -1,9 +1,25 @@
+import os
 import gzip
 import pickle
+import numpy as np
 from PIL import Image
 
 
-def main(pkl_file_path, destination_dir):
+def pickle_pngs(directory, destination_path):
+    memories = []
+    file_names = [file_name for file_name in os.listdir(directory) if file_name.endswith('.png')]
+    for i, file_name in enumerate(file_names):
+        if i % 100 == 0:
+            print('converted {} / {} images'.format(i, len(file_names)))
+        img = Image.open(file_name)
+        img.load()
+        array = np.asarray(img)
+        memories.append({'image': array, 'sensor_angles': [0, 0, 0, 0]})
+    with gzip.open(destination_path) as destination_file:
+        pickle.dump(memories, destination_file, pickle.HIGHEST_PROTOCOL)
+
+
+def create_pngs(pkl_file_path, destination_dir):
     # todo: print status
     print('opening file...')
     with gzip.open(pkl_file_path) as src_file:
@@ -19,4 +35,5 @@ def main(pkl_file_path, destination_dir):
 
 
 if __name__ == "__main__":
-    main(pkl_file_path="../grey400_original.pkl", destination_dir="../data/nao_raw/grey400/")
+    # create_pngs(pkl_file_path="../grey400_original.pkl", destination_dir="../data/nao_raw/grey400/")
+    pickle_pngs('')
