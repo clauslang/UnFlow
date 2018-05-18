a5c9fab0916cf0609732a143098c8bece0fea330
Claus <clauslang@yahoo.de>
2018-05-18 14:39:05 +0200

    rm unused method

--- src/e2eflow/nao/input.py
+++ src/e2eflow/nao/input.py
@@ -3,8 +3,5 @@ from e2eflow.core.input import Input
 
 class NaoInput(Input):
 
-    def input_test(self, hold_out_inv=None):
-        return self._input_test('nao_raw/grey400', hold_out_inv)
-
     def input_train_2012(self, hold_out_inv=None):
         return self._input_test('nao_raw/grey400', hold_out_inv)
