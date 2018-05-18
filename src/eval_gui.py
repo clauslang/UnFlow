72c6d574c4fa3df36b742dbddd0b897b54508e6f
Claus <clauslang@yahoo.de>
2018-05-18 15:45:14 +0200

    debug print

--- src/eval_gui.py
+++ src/eval_gui.py
@@ -130,16 +130,14 @@ def _evaluate_experiment(name, input_fn, data_input):
 
     with tf.Graph().as_default(): #, tf.device('gpu:' + FLAGS.gpu):
         inputs = input_fn()
-        im1, im2, input_shape = inputs[:3]
 
-        # todo: remove
-        # print(input_shape.eval())
+        print(inputs)
+
+        im1, im2, input_shape = inputs[:3]
 
         truth = inputs[3:]
 
         height, width, _ = tf.unstack(tf.squeeze(input_shape), num=3, axis=0)
-
-        # print(height, width, _)
 
         im1 = resize_input(im1, height, width, resized_h, resized_w)
         im2 = resize_input(im2, height, width, resized_h, resized_w) # TODO adapt train.py
