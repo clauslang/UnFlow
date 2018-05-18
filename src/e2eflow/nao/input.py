ebc59d7af79d919694c302d4fd2a15b3642c29b3
Claus <clauslang@yahoo.de>
2018-05-18 15:05:36 +0200

    debug input method

--- src/e2eflow/nao/input.py
+++ src/e2eflow/nao/input.py
@@ -5,3 +5,6 @@ class NaoInput(Input):
 
     def input_train_2012(self, hold_out_inv=None):
         return self._input_test('nao_raw/grey400', hold_out_inv)
+
+    def input_debug(self, hold_out_inv=None):
+        return self._input_test('nao_raw/greyfew', hold_out_inv)
