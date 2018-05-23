08730c0d83c2024cac2a351928d591321efc8fa6
Claus <clauslang@yahoo.de>
2018-05-23 18:32:45 +0200

    config

--- src/nao_img_util.py
+++ src/nao_img_util.py
@@ -36,4 +36,4 @@ def create_pngs(pkl_file_path, destination_dir):
 
 if __name__ == "__main__":
     # create_pngs(pkl_file_path="../grey400_original.pkl", destination_dir="../data/nao_raw/grey400/")
-    pickle_pngs('../out/css_nao_thr5/', '../grey400_flow_thr5.pkl')
+    pickle_pngs('../out/css_nao_flowintensity/', '../grey400_flow_intensity.pkl')
